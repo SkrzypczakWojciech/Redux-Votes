d01cf61510701bce6a28a5a9872e91d72bba7b7f
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-09-12 18:33:53 +0200

    init whole project

---
+++ src/actions.js
@@ -0,0 +1,55 @@
+import uuid from 'uuid';
+import React from 'react';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+dispatch(addComment('nowy komentarz!'));
+
+function removeComment(id) {
+    return {
+    	type: REMOVE_COMMENT,
+        id
+    }
+}
+
+dispatch(removeComment(3));
+
+function editComment(text,id) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id
+    }
+}
+
+dispatch(editComment('edytuje komentarz',3));
+
+function thumbUp(id) {
+    return {
+        type:THUMB_UP_COMMENT,
+        id
+    }
+}
+
+dispatch(thumbUp(3));
+
+function thumbDown(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
+    }
+}
+
+dispatch(thumbDown(3));
---
+++ src/comments.js
@@ -0,0 +1,52 @@
+import {ADD_COMMENT} from './actions'
+import {EDIT_COMMENT} from './actions'
+import {REMOVE_COMMENT} from './actions'
+import {THUMB_UP} from './actions'
+import {THUMB_DOWN} from './actions'
+
+function comments(state = [], action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return [{
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }
+                , ...state.comments]          
+        case EDIT_COMMENT:
+        	return state.comments.map(comment => {
+        		if(comment.id === action.id){
+        			return {
+                        id: comment.id,
+                        text: action.text,
+                        votes: comment.votes
+                    }};
+        		return comment;
+            })
+        case REMOVE_COMMENT:
+            return {
+                comments: state.comments.filter(comment => comment.id !== action.id)
+            };
+        case THUMB_UP:
+        	const up = state.comments.map(comment => {
+        		if(comment.id === action.id){
+        			return {
+        				id: comment.id,
+        				text: comment.text,
+        				votes: comment.votes +1	
+        			}};
+        		return comment;
+        	})
+        case THUMB_DOWN:
+        	const down = state.comments.map(comment => {
+        		if(comment.id === action.id){
+        			return {
+        				id: comment.id,
+        				text: comment.text,
+        				votes: comment.votes -1
+        			}};
+        		return comment;
+        	})
+        }};
+
+export default comments;
